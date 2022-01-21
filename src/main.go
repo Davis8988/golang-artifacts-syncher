0b769ad3697eb45c649317d088b67ab6bd540700
David Yair <yair.david@elbitsystems.com>
2022-01-21 12:48:13 +0200

    Added "flag" import

--- src/main.go
+++ src/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 )
 
+var (
+	file   = flag.String("file", "", "file-name")
+	count  = flag.Int("count", 2, "count params")
+	repeat = flag.Bool("repeat", false, "Repeat execution")
+)
+
 func main() {
+	flag.Parse()
+
+	fmt.Println("file name: ", *file)
+	fmt.Println("count: ", *count)
+	fmt.Println("repeat: ", *repeat)
+
 	fmt.Println("Hello World")
 }
