662ed988d3ba462eee62b4c11c9ef4df8eb3a9db
David Yair <yair.david@elbitsystems.com>
2022-01-22 12:15:32 +0200

    Added func: prepareSearchUrlsArray()

--- src/main.go
+++ src/main.go
@@ -13,12 +13,10 @@ var (
 	serversUrlsStr string
 	reposNamesStr string
 	packagesNamesStr string
-	packagesVersionsStr string
 
 	serversUrlsArr [] string
 	reposNamesArr [] string
 	packagesNamesArr [] string
-	packagesVersionsArr [] string
 )
 
 // Attempts to resolve an environment variable, 
@@ -38,19 +36,16 @@ func initVars() {
 	serversUrlsStr = getenv("SERVERS_URLS_STR", "")
 	reposNamesStr = getenv("REPOS_NAMES_STR", "")
 	packagesNamesStr = getenv("PACKAGES_NAMES_STR", "")
-	packagesVersionsStr = getenv("PACKAGES_VERSIONS_STR", "")
 }
 
 func printVars() {
 	log.Printf("SERVERS_URLS_STR: '%s'", serversUrlsStr)
 	log.Printf("REPOS_NAMES_STR: '%s'", reposNamesStr)
 	log.Printf("PACKAGES_NAMES_STR: '%s'", packagesNamesStr)
-	log.Printf("PACKAGES_VERSIONS_STR: '%s'", packagesVersionsStr)
 	
 	log.Printf("serversUrlsArr: %v", serversUrlsArr)
 	log.Printf("reposNamesArr: %v", reposNamesArr)
 	log.Printf("packagesNamesArr: %v", packagesNamesArr)
-	log.Printf("packagesVersionsArr: %v", packagesVersionsArr)
 }
 
 func parseArgs() {
@@ -63,13 +58,25 @@ func updateVars() {
 	serversUrlsArr = strings.Split(serversUrlsStr, ";")
 	reposNamesArr = strings.Split(reposNamesStr, ";")
 	packagesNamesArr = strings.Split(packagesNamesStr, ";")
-	packagesVersionsArr = strings.Split(packagesVersionsStr, ";")
+}
+
+func prepareSearchUrlsArray() []string {
+	log.Printf("Preparing search packages urls")
+	searchOptionsUrl := "Search()?"
+	for _, serverUrl := range serversUrlsArr {
+		for _, repoName := range reposNamesArr {
+			for _, pkgName := range packagesNamesArr {
+				searchUrlsArr = append(searchUrlsArr, serverUrl + "/" + repoName + "/" + searchOptionsUrl + "id='" + pkgName + "'")
+			}
+		}
+	}
 }
 
 func searchSpecifiedPackages() []string {
 	var foundPackagesArr []string
-	log.Printf("Searching for packages")
+	var searchUrlsArr []string
 	
+	log.Printf("Search array: %v", searchUrlsArr)
 	return foundPackagesArr 
 }
 
