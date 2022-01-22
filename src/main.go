c8dd4dceb791cfccd444b6582b598cab2a710649
David Yair <yair.david@elbitsystems.com>
2022-01-22 16:08:21 +0200

    Added package versions param env

--- src/main.go
+++ src/main.go
@@ -13,10 +13,12 @@ var (
 	serversUrlsStr string
 	reposNamesStr string
 	packagesNamesStr string
+	packagesVersionsStr string
 
 	serversUrlsArr [] string
 	reposNamesArr [] string
 	packagesNamesArr [] string
+	packagesVersionsArr [] string
 )
 
 // Attempts to resolve an environment variable, 
@@ -36,16 +38,19 @@ func initVars() {
 	serversUrlsStr = getenv("SERVERS_URLS_STR", "")
 	reposNamesStr = getenv("REPOS_NAMES_STR", "")
 	packagesNamesStr = getenv("PACKAGES_NAMES_STR", "")
+	packagesVersionsStr = getenv("PACKAGES_VERSIONS_STR", "")
 }
 
 func printVars() {
 	log.Printf("SERVERS_URLS_STR: '%s'", serversUrlsStr)
 	log.Printf("REPOS_NAMES_STR: '%s'", reposNamesStr)
 	log.Printf("PACKAGES_NAMES_STR: '%s'", packagesNamesStr)
+	log.Printf("PACKAGES_VERSIONS_STR: '%s'", packagesVersionsStr)
 	
 	log.Printf("serversUrlsArr: %v", serversUrlsArr)
 	log.Printf("reposNamesArr: %v", reposNamesArr)
 	log.Printf("packagesNamesArr: %v", packagesNamesArr)
+	log.Printf("packagesVersionsArr: %v", packagesVersionsArr)
 }
 
 func validateEnv() {
