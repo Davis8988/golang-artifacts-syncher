558d3de3cde31223a1072d65e07a98688aab93c2
David Yair <yair.david@elbitsystems.com>
2022-01-29 18:58:28 +0200

    Assigning: httpRequestHeadersMap[headerKey] = headerValue

--- src/helpers/helpers.go
+++ src/helpers/helpers.go
@@ -75,7 +75,7 @@ func ConvertSyncedMapToString(synchedMap sync.Map) string {
 func ParseHttpHeadersStrToMap(httpRequestHeadersStr string) map[string]string {
     httpRequestHeadersMap := make(map[string] string)
     if len(httpRequestHeadersStr) <= 1 {return httpRequestHeadersMap}
-    
+
     tempHeadersPairsArr := make([]string, 0, 6)
     tempPairArr := make([]string, 0, 2)
     tempHeadersPairsArr = strings.Split(httpRequestHeadersStr, ";")
@@ -88,6 +88,7 @@ func ParseHttpHeadersStrToMap(httpRequestHeadersStr string) map[string]string {
         }
         headerKey := tempPairArr[0]
         headerValue := tempPairArr[1]
+        httpRequestHeadersMap[headerKey] = headerValue
     }
 }
 
