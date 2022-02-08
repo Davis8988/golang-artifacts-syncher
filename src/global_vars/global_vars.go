c2c7cb3cc2f3bc3dc79a688698efed92e895324d
David Yair <Davismarsel@gmail.com>
2022-02-08 14:59:57 +0200

    Added import ( 	"sync" ) to: global_vars pkg && Removed log vars

--- src/global_vars/global_vars.go
+++ src/global_vars/global_vars.go
@@ -1,14 +1,10 @@
 package global_vars
 
+import (
+	"sync"
+)
 
 var (
-    // Log
-    LogInfo = log.New(os.Stdout, "\u001b[37m", log.LstdFlags)
-    LogWarning = log.New(os.Stdout, "\u001b[33mWARNING: ", log.LstdFlags)
-    LogError = log.New(os.Stdout, "\u001b[35m Error: \u001B[31m", log.LstdFlags)
-    LogDebug = log.New(os.Stdout, "\u001b[36mDebug: ", log.LstdFlags)
-
-
     // Locks
     convertSyncedMapToString_Lock sync.RWMutex
     appendPkgDetailsArr_Lock sync.RWMutex
