e4548e2b09a51b9f67c095f2d015e8df4438dbd6
David Yair <Davismarsel@gmail.com>
2022-02-08 14:10:22 +0200

    Added 2 funcs to nuget|_cli pkg

--- src/nuget_cli/nuget_cli.go
+++ src/nuget_cli/nuget_cli.go
@@ -1,20 +1,13 @@
-package nexus3_adapter
+package nuget_cli
 
 import (
 	"os"
 )
 
-// Attempts to resolve an environment variable, 
-//  with a default value if it's empty
-func Getenv(key, fallback string) string {
-    value := os.Getenv(key)
-    if len(value) == 0 {
-        return fallback
-    }
-    return value
+func pushNugetPackage() {
+
 }
 
-func IsStrArrayEmpty(arrToCheck []string) bool {
-	return len(arrToCheck) == 0
-}
+func downloadNugetPackage() {
 
+}
