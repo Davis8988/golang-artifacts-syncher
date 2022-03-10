3449116130a513d95360652b9d71a85250e0f245
David Yair <yair.david@elbitsystems.com>
2022-03-10 14:16:46 +0200

    Printings

--- src/nuget_cli/nuget_cli.go
+++ src/nuget_cli/nuget_cli.go
@@ -263,7 +263,7 @@ func UploadDownloadedPackages(downloadedPkgsArr []global_structs.DownloadPackage
 		mylog.Logger.Warnf("No packages to upload given")
 		return
 	}
-	mylog.Logger.Infof("Uploading %d downloaded packages to servers: %v", len(downloadedPkgsArr), global_vars.DestServersUrlsArr)
+	mylog.Logger.Infof("Uploading %d downloaded packages to servers: %v  in groups of: %d", len(downloadedPkgsArr), global_vars.PackagesMaxConcurrentUploadCount)
 	if len(global_vars.DestServersUrlsArr) == 0 {
 		mylog.Logger.Warnf("No servers to upload to were given - skipping uploading of: %d packages", len(downloadedPkgsArr))
 		return
