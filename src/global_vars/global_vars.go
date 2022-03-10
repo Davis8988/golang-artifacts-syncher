723287e97816cfe085c3b461bd80d7a0e6433dbf
David Yair <yair.david@elbitsystems.com>
2022-03-10 20:54:59 +0200

    Addefd var: PackagesDownloadLimitCount

--- src/global_vars/global_vars.go
+++ src/global_vars/global_vars.go
@@ -24,6 +24,7 @@ var (
     DownloadPkgsDirPath          string
     HttpRequestTimeoutSecondsInt int
     SearchPackagesUrlSkipGroupCount int  // Used for URL searching requests of Nuget pkgs - Can't query for all at once, need to query multiple times and skip previous results.
+    PackagesDownloadLimitCount int  
     PackagesMaxConcurrentDownloadCount int  
     PackagesMaxConcurrentUploadCount int  
     LogLevel string
